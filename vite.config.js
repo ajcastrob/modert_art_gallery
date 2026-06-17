d18de9fd41c6e79c11dd90d60f098f7bfbd0ebd8
José Castro <a.j.castro.b@gmail.com>
2026-06-16 19:08:58 -0500

    agregando path de location.html

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/modert_art_gallery/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        location: resolve(__dirname, 'pages/location.html'),
+      },
+    },
+  },
 });
